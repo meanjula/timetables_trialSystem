a3eef612dfa576a425504b9d493def38ca50bd6f
meanjula <anjula.ghale@gmail.com>
2022-03-25 18:51:18 +0200

    search address using geocoding

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import { WalkIcon } from "./components/style";
 import { CircularProgress } from "@mui/material";
 import { ArrowIcon } from "./components/style";
 import { Alarm } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
 const generateQuery = (from, to) => {
   return `
  {
     plan(
       fromPlace: "${from}",
-      toPlace:"${to}" ,
-      numItineraries: 5,
-      
+      toPlace:"${to}" ,  
     ) {
       
       itineraries{
@@ -68,20 +67,35 @@ function App() {
 
   const handleFrom = (event) => {
     setValueFrom(event.target.value);
-    console.log(event.target.value);
+    getCoordinates(event.target.value);
   };
   const handleTo = (event) => {
     event.preventDefault();
     setValueTo(event.target.value);
+    getCoordinates(event.target.value);
   };
+  const getCoordinates = (address) => {
+    fetch(
+      `https://api.digitransit.fi/geocoding/v1/search?text=${address}&size=1`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const coordinates = data.features[0].geometry.coordinates;
+        const stringCoordinates = coordinates.reverse().join();
+        const region = data.features[0].properties.locality;
+        const fullAddress = `${address},${region}::${stringCoordinates}`;
 
+        setValueFrom(fullAddress);
+        setValueTo(fullAddress);
+      });
+  };
   const getData = (from, to) => {
     fetch("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: generateQuery(from, to) }),
     })
-      .then((response) => response.json(console.log(response)))
+      .then((response) => response.json())
       .then((data) => {
         setDataf(data.data.plan);
         setIsLoading(false);
@@ -92,13 +106,17 @@ function App() {
     getData(valueFrom, valueTo);
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getData(valueFrom, valueTo);
+  };
+
   return (
     <div className="App">
       <h1>Timetables</h1>
       <div className="text-container">
         <label>
           <input
-            placeholder="Lapinlahdenkatu , Helsinki::60.1675,24.92301"
             onChange={handleFrom}
             className="from tfield"
             form={valueFrom}
@@ -107,53 +125,53 @@ function App() {
         <label>
           <input onChange={handleTo} className="to textfield" to={valueTo} />
         </label>
-        <button onClick={getData}>submit</button>
+        <Button onClick={handleSubmit} className="button">
+          submit
+        </Button>
       </div>
       {isLoading && <CircularProgress />}
-      <div>
-        {!isLoading && (
-          <Container className="container">
-            <div>
-              {dataf.itineraries.map((itenary) => (
-                <div key={itenary.duration} className="card">
-                  <div className="walk-distance">
-                    <WalkIcon />
-                    <p>{(itenary.walkDistance / 100).toFixed(2)}km</p>
-                  </div>
-                  <div className="itenaries">
-                    {itenary.legs.map((leg) => (
-                      <div key={leg.lat} className="itenary-box">
-                        <div className="icon-box">
-                          <div className="icon">
-                            {leg.mode === "WALK" ? (
-                              <WalkIcon className="walk-icon" />
-                            ) : leg.mode === "BUS" ? (
-                              <BusIcon />
-                            ) : (
-                              <SubwayIcon />
-                            )}
-                          </div>
-                          <ArrowIcon />
+      {!isLoading && (
+        <Container className="container">
+          <div>
+            {dataf.itineraries.map((itenary) => (
+              <div key={itenary.duration} className="card">
+                <div className="walk-distance">
+                  <WalkIcon />
+                  <p>{(itenary.walkDistance / 100).toFixed(2)}km</p>
+                </div>
+                <div className="itenaries">
+                  {itenary.legs.map((leg) => (
+                    <div key={leg.lat} className="itenary-box">
+                      <div className="icon-box">
+                        <div className="icon">
+                          {leg.mode === "WALK" ? (
+                            <WalkIcon className="walk-icon" />
+                          ) : leg.mode === "BUS" ? (
+                            <BusIcon />
+                          ) : (
+                            <SubwayIcon />
+                          )}
                         </div>
-
-                        <p>
-                          {new Date(leg.startTime).toISOString().slice(11, 16)}-
-                          {new Date(leg.endTime).toISOString().slice(11, 16)}
-                        </p>
-                        <p>Via {leg.from.name}</p>
+                        <ArrowIcon />
                       </div>
-                    ))}
-                    <div className="totalTime-box">
-                      <Alarm />
-                      <p>{itenary.duration / 100} min</p>
+
+                      <p>
+                        {new Date(leg.startTime).toISOString().slice(11, 16)}-
+                        {new Date(leg.endTime).toISOString().slice(11, 16)}
+                      </p>
+                      <p>Via {leg.from.name}</p>
                     </div>
+                  ))}
+                  <div className="totalTime-box">
+                    <Alarm />
+                    <p>{itenary.duration / 100} min</p>
                   </div>
                 </div>
-              ))}
-            </div>
-          </Container>
-        )}
-      </div>
+              </div>
+            ))}
+          </div>
+        </Container>
+      )}
     </div>
   );
 }
