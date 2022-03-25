eca7e8f5a0ee28841f200c47c8a34bd99a7cc319
meanjula <anjula.ghale@gmail.com>
2022-03-25 13:14:28 +0200

    added the button for fetching dynamic data

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import { CircularProgress } from "@mui/material";
 import { ArrowIcon } from "./components/style";
 import { Alarm } from "@mui/icons-material";
 
-const Query = `
+const generateQuery = (from, to) => {
+  return `
  {
     plan(
-      fromPlace: "Lapinlahdenkatu , Helsinki::60.1675,24.92301",
-      toPlace: "Roihuvuori,Helsinki::60.1999992 25.0666664",
+      fromPlace: "${from}",
+      toPlace:"${to}" ,
       numItineraries: 5,
       
     ) {
@@ -53,54 +54,8 @@ const Query = `
   }
   
   }`;
+};
 
-const queryFetch = `
-{
- plans($to:to){
-     plan(
-       fromPlace: "Lapinlahdenkatu , Helsinki::60.1675,24.92301",
-       toPlace: $to,
-       numItineraries: 5,  
-     ) {   
-       itineraries{
-       walkDistance,
-       duration,
-       legs {
-         mode
-         startTime
-         endTime
-         from {
-         lat
-         lon
-         name
-         stop {
-           code
-           name
-         }
-         },
-         to {
-         lat
-         lon
-         name
-         },
-         agency {
-         gtfsId
-             name
-         },
-         distance
-         legGeometry {
-         length
-         points
-         }
-       }
-       }
-     }  
-   }
-  }
- 
-  }
-  
- }`;
 function App() {
   const [valueFrom, setValueFrom] = useState(
     "Lapinlahdenkatu , Helsinki::60.1675,24.92301"
@@ -120,11 +75,11 @@ function App() {
     setValueTo(event.target.value);
   };
 
-  const getData = () => {
+  const getData = (from, to) => {
     fetch("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query: Query }),
+      body: JSON.stringify({ query: generateQuery(from, to) }),
     })
       .then((response) => response.json(console.log(response)))
       .then((data) => {
@@ -134,25 +89,9 @@ function App() {
       });
   };
   useEffect(() => {
-    getData();
+    getData(valueFrom, valueTo);
   }, []);
 
-  useEffect(() => {
-    fetch("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: queryFetch,
-        variables: {
-          to: valueTo,
-        },
-      }),
-    })
-      .then((response) => response.json(console.log(response)))
-      .then((data) => {
-        console.log(data);
-      });
-  }, [valueTo]);
   return (
     <div className="App">
       <h1>Timetables</h1>
@@ -168,50 +107,53 @@ function App() {
         <label>
           <input onChange={handleTo} className="to textfield" to={valueTo} />
         </label>
+        <button onClick={getData}>submit</button>
       </div>
       {isLoading && <CircularProgress />}
-      {!isLoading && (
-        <Container className="container">
-          <div>
-            {dataf.itineraries.map((itenary) => (
-              <div key={itenary.duration} className="card">
-                <div className="walk-distance">
-                  <WalkIcon />
-                  <p>{(itenary.walkDistance / 100).toFixed(2)}km</p>
-                </div>
-                <div className="itenaries">
-                  {itenary.legs.map((leg) => (
-                    <div key={leg.lat} className="itenary-box">
-                      <div className="icon-box">
-                        <div className="icon">
-                          {leg.mode === "WALK" ? (
-                            <WalkIcon className="walk-icon" />
-                          ) : leg.mode === "BUS" ? (
-                            <BusIcon />
-                          ) : (
-                            <SubwayIcon />
-                          )}
+      <div>
+        {!isLoading && (
+          <Container className="container">
+            <div>
+              {dataf.itineraries.map((itenary) => (
+                <div key={itenary.duration} className="card">
+                  <div className="walk-distance">
+                    <WalkIcon />
+                    <p>{(itenary.walkDistance / 100).toFixed(2)}km</p>
+                  </div>
+                  <div className="itenaries">
+                    {itenary.legs.map((leg) => (
+                      <div key={leg.lat} className="itenary-box">
+                        <div className="icon-box">
+                          <div className="icon">
+                            {leg.mode === "WALK" ? (
+                              <WalkIcon className="walk-icon" />
+                            ) : leg.mode === "BUS" ? (
+                              <BusIcon />
+                            ) : (
+                              <SubwayIcon />
+                            )}
+                          </div>
+                          <ArrowIcon />
                         </div>
-                        <ArrowIcon />
-                      </div>
 
-                      <p>
-                        {new Date(leg.startTime).toISOString().slice(11, 16)}-
-                        {new Date(leg.endTime).toISOString().slice(11, 16)}
-                      </p>
-                      <p>Via {leg.from.name}</p>
+                        <p>
+                          {new Date(leg.startTime).toISOString().slice(11, 16)}-
+                          {new Date(leg.endTime).toISOString().slice(11, 16)}
+                        </p>
+                        <p>Via {leg.from.name}</p>
+                      </div>
+                    ))}
+                    <div className="totalTime-box">
+                      <Alarm />
+                      <p>{itenary.duration / 100} min</p>
                     </div>
-                  ))}
-                  <div className="totalTime-box">
-                    <Alarm />
-                    <p>{itenary.duration / 100} min</p>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </Container>
-      )}
+              ))}
+            </div>
+          </Container>
+        )}
+      </div>
     </div>
   );
 }
